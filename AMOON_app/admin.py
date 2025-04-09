9ecd0cb1771e800e78bcdfb721491f8b12e557a4
Yousef Ali <620220263@fa-hists.edu.eg>
2025-04-09 17:09:01 +0200

    Add files via upload

---
+++ AMOON_app/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser, Company ,EmployeeActivity ,Training
+from django.contrib.auth import get_user_model
+
+
+
+if admin.site.is_registered(User):
+    admin.site.unregister(User)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'company', 'is_staff')
+    search_fields = ('username', 'email', 'company__name')
+    list_filter = ('company', 'is_staff', 'is_superuser')
+
+CustomUser = get_user_model()
+
+# ✅ تجنب تسجيل CustomUser مرتين
+if not admin.site.is_registered(CustomUser):
+    admin.site.register(CustomUser, CustomUserAdmin)
+
+if not admin.site.is_registered(Company):
+    admin.site.register(Company)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'employee_count', 'max_employee_count')
+
+
+
+    search_fields = ('name', 'code')
+
